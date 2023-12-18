f9e74ac1eea1469d59ca57070f9002575a3c9928
Rajeev Singh <rajeevsinghece49@gmail.com>
2023-12-18 14:27:12 +0530

    API to Serve Image

--- src/main/java/com/imageProject/FileDemo/controllers/FileController.java
+++ src/main/java/com/imageProject/FileDemo/controllers/FileController.java
@@ -2,17 +2,18 @@ package com.imageProject.FileDemo.controllers;
 
 import com.imageProject.FileDemo.payload.FileResponse;
 import com.imageProject.FileDemo.services.FileService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/file")
@@ -35,5 +36,15 @@ public class FileController {
         }
         return new ResponseEntity<>(new FileResponse(fileName,"Image is uploaded successfully"), HttpStatus.OK);
     }
+
+     //method to serve file
+    @GetMapping(value="/profiles/{imageName}",produces=MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response) throws IOException {
+        InputStream  resource = this.fileService.getResource(path,imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource,response.getOutputStream());
+    }
+
+    //url = http://localhost:8080/file/profiles/9c999ff8-b642-4850-81f6-ef00f480b727.png
 
 }
--- src/main/java/com/imageProject/FileDemo/services/impl/FileServiceImpl.java
+++ src/main/java/com/imageProject/FileDemo/services/impl/FileServiceImpl.java
@@ -4,8 +4,7 @@ import com.imageProject.FileDemo.services.FileService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -38,5 +37,12 @@ public class FileServiceImpl implements FileService {
         Files.copy(file.getInputStream(), Paths.get(filePath));
 
         return name;
+    }
+
+    @Override
+    public InputStream getResource(String path, String fileName) throws FileNotFoundException {
+       String fullPath=path+File.separator+fileName;
+       InputStream is = new FileInputStream(fullPath);
+        return is;
     }
 }
